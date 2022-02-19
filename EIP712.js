892ed69736d807da89a208b303ba1385b7fa8ecb
LeviHHH <LeviHHH@protonmail.com>
2022-02-19 16:33:46 +0800

    init

---
+++ EIP712.js
@@ -0,0 +1,42 @@
+const { getMessage } = require('eip-712');
+const ethers = require('ethers')
+
+const EIP712Domain = [
+    { name: 'name', type: 'string' },
+    { name: 'version', type: 'string' },
+    { name: 'chainId', type: 'uint256' },
+    { name: 'verifyingContract', type: 'address' }
+]
+const Authorize = [
+    { name: 'token', type: 'address' },
+    { name: 'tokenID', type: 'uint256' },
+    { name: 'targetChainID', type: 'uint256' },
+    { name: 'targetIPPool', type: 'address' },
+    { name: 'nonce', type: 'uint256' },
+    //{ name: 'deadline', type: 'uint256' }
+]
+
+const MessageType = {
+    Authorize
+}
+
+module.exports = {
+    verifyTypeData(primaryType, domain, message, signature) {
+        const typedData = {
+            types: {
+                EIP712Domain,
+                [primaryType]: MessageType[primaryType]
+            },
+            domain,
+            primaryType,
+            message
+        }
+        const digest = Buffer.from(getMessage(typedData, true)).toString('hex')
+        const account = ethers.utils.recoverAddress(`0x${digest}`, {
+            r: signature.slice(0, 2+(32*2)),
+            s: `0x${signature.slice(2+(32*2), 2+(32*2)*2)}`,
+            v: parseInt(signature.slice(-2), 16)
+        })
+        return account
+    }
+}
